63caac1b6b8ad8825fb53ddd5b784d1718ca1dac
Franciso Javier Acosta Martinez <116164601+chico23018@users.noreply.github.com>
2023-09-14 17:53:36 +0200

    aggiunto ticket

--- universita/src/main/java/implement/Test.java
+++ universita/src/main/java/implement/Test.java
@@ -7,21 +7,23 @@ public class Test {
 
 	public static void main(String[] args) throws SQLException {
 
-		//Prenotazione ap = new Prenotazione();
+		Ticket ap = new Ticket();
 		
-		//ap.inserire(100, "Ingegneria del software"); // inserisce con 7
+		
+		//ap.inserire(100,2,"ho famme"); // inserisce con 7
+		
 		//ap.modifica(4,"Alessia", "Crispo", "pass", "123465");
-		// ap.elimina(10);
-		
-		/*ResultSet rs = ap.lista();
+		//ap.elimina(3);
+	/*	
+		ResultSet rs = ap.lista();
 		while(rs.next())
-			System.out.println("Lista : " +rs.getString("nome"));
+			System.out.println("Lista : " +rs.getString("richiesta"));
 		
 		rs = ap.ricerca(1);
 		rs.next();
-		System.out.println("\nRicerca : " +rs.getString("nome"));
-		*/
+		System.out.println("\nRicerca : " +rs.getString("richiesta"));
 		
+		*/
 
 	}
 }
