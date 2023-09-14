252ed04088994a2862fa4ff4b1c8918e0be1cb9a
Franciso Javier Acosta Martinez <116164601+chico23018@users.noreply.github.com>
2023-09-14 14:46:33 +0200

    aggiunto Appello e Iscrizione

---
+++ universita/src/main/java/implem/Appello.java
@@ -0,0 +1,37 @@
+package implem;
+
+import java.sql.ResultSet;
+
+public class Appello {
+
+	
+	public void inserire() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void modifica() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void elimina() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public ResultSet lista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public ResultSet ricerca() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
