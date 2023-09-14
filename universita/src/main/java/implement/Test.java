2429b39092b2e80111c452c696cd1fb8c25daae8
Franciso Javier Acosta Martinez <116164601+chico23018@users.noreply.github.com>
2023-09-14 17:26:49 +0200

    aggiunto professore

--- universita/src/main/java/implement/Test.java
+++ universita/src/main/java/implement/Test.java
@@ -6,21 +6,19 @@ import java.sql.SQLException;
 public class Test {
 
 	public static void main(String[] args) throws SQLException {
-		Iscrizione ap = new Iscrizione();
-		
+		Professore ap = new Professore();
+		//ap.inserire("Alessia", "Crispo", "pass", "1234");
 		//ap.inserire(100, "Ingegneria del software"); // inserisce con 7
-		//ap.modifica(10, 101, "Ingegneria del software");
+		//ap.modifica(4,"Alessia", "Crispo", "pass", "123465");
 		// ap.elimina(10);
 		
 		/*ResultSet rs = ap.lista();
 		while(rs.next())
-			System.out.println("Lista : " +rs.getString("nome_mat"));
-		*/
+			System.out.println("Lista : " +rs.getString("nome"));
 		
-		/*
 		rs = ap.ricerca(1);
 		rs.next();
-		System.out.println("\nRicerca : " +rs.getString("nome_mat"));
+		System.out.println("\nRicerca : " +rs.getString("nome"));
 		*/
 		
 
