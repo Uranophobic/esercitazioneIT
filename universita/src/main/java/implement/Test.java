ce390b16bbd29bd9cb576f502625abb32fc6cc6a
nameisalfio <alfio.spoto@outlook.it>
2023-09-14 17:17:56 +0200

    Implements prenotazione

--- universita/src/main/java/implement/Test.java
+++ universita/src/main/java/implement/Test.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class Test {
 
 	public static void main(String[] args) throws SQLException {
-		Iscrizione ap = new Iscrizione();
+		//Prenotazione ap = new Prenotazione();
 		
 		//ap.inserire(100, "Ingegneria del software"); // inserisce con 7
 		//ap.modifica(10, 101, "Ingegneria del software");
