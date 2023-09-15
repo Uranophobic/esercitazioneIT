d8cc0d91d771f135b4033170783bd2cade2497e0
Franciso Javier Acosta Martinez <116164601+chico23018@users.noreply.github.com>
2023-09-15 15:14:04 +0200

    modificato  stampaStudente

--- universita/src/main/java/control/Prenotazione.java
+++ universita/src/main/java/control/Prenotazione.java
@@ -18,81 +18,64 @@ import implement.Appello;
 import implement.Lezione;
 import implement.Query;
 
-
 /**
  * Servlet implementation class Prenotazione
  */
 @WebServlet("/Prenotazione")
 public class Prenotazione extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Prenotazione() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Prenotazione() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String materia=request.getParameter("materia");
-		Lezione lezione  = new Lezione();
+		String materia = request.getParameter("materia");
+		Lezione lezione = new Lezione();
 		Query query = new Query();
-		Connection conn= null;
+		Connection conn = null;
 		try {
-			
+
 			int idcorso = Integer.parseInt(materia);
-			
-			
-//			
-//			conn = Connessione.getInstance().getConnection();
-//			
-//			PreparedStatement smt1=conn.prepareStatement("select materia from lezione where idcorso=CAST(? AS UNSIGNED INTEGER)");
-//			smt1.setString(1, materia);
-//			ResultSet rs1 = smt1.executeQuery();
-			
+
 			ResultSet rs1 = lezione.ricerca(idcorso);
 			rs1.next();
-			
-			
-			//restituisce il nome della materia che vogliamo stampare
-			
-			String nomeMateria=rs1.getString("materia");
-			
-//			PreparedStatement smt= conn.prepareStatement("select idAppello,Data from appello where materia=CAST(? AS UNSIGNED INTEGER)");
-//			smt.setString(1,nomeMateria);
-//			
-			//ResultSet rs= smt.executeQuery();//questo resultset mi prende appelli e date richiesti nella prepared
-			
-			ResultSet rs=	query.getResult("select idAppello,Data from appello where materia='"+nomeMateria+"'");
-			RequestDispatcher rd=request.getRequestDispatcher("studente.jsp");
+
+			// restituisce il nome della materia che vogliamo stampare
+
+			String nomeMateria = rs1.getString("materia");
+
+			ResultSet rs = query.getResult("select idAppello,Data from appello where materia='" + nomeMateria + "'");
+
+			RequestDispatcher rd = request.getRequestDispatcher("studente.jsp");
+
 			request.setAttribute("materia", nomeMateria);
 			request.setAttribute("elenco_appelli", rs);
 			rd.forward(request, response);
-			
-			
-			
-		}catch (SQLException e) {
+
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
-		}
-	
-	
-	
-	
 	}
-	
 
-
+}
--- universita/src/main/java/control/StampaStudenti.java
+++ universita/src/main/java/control/StampaStudenti.java
@@ -13,9 +13,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import implement.Appello;
 import implement.DataFormato;
+import implement.Lezione;
 import implement.Query;
-
 
 /**
  * Servlet implementation class StampaStudenti
@@ -23,7 +24,9 @@ import implement.Query;
 @WebServlet("/StampaStudenti")
 public class StampaStudenti extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	Query query = new Query();
+	private Query query = new Query();
+	private Appello appello = new Appello();
+	private Lezione lezione = new Lezione();
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -34,66 +37,54 @@ public class StampaStudenti extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
-		String idAppello= request.getParameter("ID_appello");
-		Connection conn=null;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String idAppello = request.getParameter("ID_appello");
+		Connection conn = null;
+		int id = Integer.parseInt(idAppello);
 
 		try {
-			conn = Connessione.getInstance().getConnection();
-			/*PreparedStatement smt1=conn.prepareStatement("select stud_prenotato from prenotazione where app_prenotato=CAST(? AS UNSIGNED INTEGER)");
-			smt1.setString(1, idAppello);
-			ResultSet rs1=smt1.executeQuery();
-			while(rs1.next()) {
 
+			ResultSet rs1 = query.getResult(
+					"select distinct nome,cognome,Matricola from studente join (appello join prenotazione on CAST("
+							+ idAppello + " AS UNSIGNED INTEGER)=id_app) on Matricola=matricola_stud");
 
-			String stud=rs1.getString(1);*/
-			PreparedStatement smt= conn.prepareStatement("select Materia,Data from appello where idAppello=CAST(? AS UNSIGNED INTEGER)");
-			smt.setString(1, idAppello);
-			ResultSet rs=smt.executeQuery();
+			ResultSet rs = appello.ricerca(id);
+
 			rs.next();
-			String Materia= rs.getString("Materia");
-			String Data1= rs.getString("Data");
-			DataFormato Data= new DataFormato();
-			
-			
-			PreparedStatement smt2= conn.prepareStatement("select materia from lezione where materia='"+Materia+"'");
-		//	smt2.setString(1, Materia);
-			ResultSet rs2= smt2.executeQuery();
+			String Materia = rs.getString("Materia");
+			String Data1 = rs.getString("Data");
+			DataFormato Data = new DataFormato();
+
+			ResultSet rs2 = query.getResult("select materia from lezione where materia='" + Materia + "'");
 			rs2.next();
-			String nomeMateria= rs2.getString(1);
-			
-//			PreparedStatement smt1= conn.prepareStatement("select nome,cognome,Matricola from studente join (appello join prenotazione on CAST(? AS UNSIGNED INTEGER)=id_app) on Matricola=matricola_stud");
-//			smt1.setString(1, idAppello);
-//			ResultSet rs1=smt1.executeQuery();
-			
-			ResultSet rs1= query.getResult("select distinct nome,cognome,Matricola from studente join (appello join prenotazione on CAST("+idAppello+" AS UNSIGNED INTEGER)=id_app) on Matricola=matricola_stud");
-			RequestDispatcher rd= request.getRequestDispatcher("professore.jsp");
-			request.setAttribute("Materia",nomeMateria);
-			request.setAttribute("Data",Data.dataIngToIta(Data1));
+			String nomeMateria = rs2.getString(1);
+
+			RequestDispatcher rd = request.getRequestDispatcher("professore.jsp");
+			request.setAttribute("Materia", nomeMateria);
+			request.setAttribute("Data", Data.dataIngToIta(Data1));
 			request.setAttribute("elenco_studenti", rs1);
 			rd.forward(request, response);
 
-
-		} catch (SQLException | ClassNotFoundException e) {
+		} catch (SQLException e) {
 
 			e.printStackTrace();
 		}
 
-
-
 	}
-
-	
 
 }
